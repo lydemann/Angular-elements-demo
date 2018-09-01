facad9c307ba8b7b914908f60c937eeef07bd449
Christian Lüdemann <christianlydemann@gmail.com>
2018-09-01 16:50:27 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import { NgModule, Injector } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
+import { HelloComponent } from './hello/hello.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      HelloComponent
+   ],
+   imports: [
+      BrowserModule
+   ],
+   providers: [],
+   entryComponents: [HelloComponent]
 })
-export class AppModule { }
+export class AppModule {
+    /**
+     *
+     */
+    constructor(private injector: Injector) {
+    }
+
+    ngDoBootstrap() {
+        const helloComponentEl = createCustomElement(HelloComponent, { injector: this.injector });
+
+        customElements.define('hello-custom-element', helloComponentEl);
+    }
+}
